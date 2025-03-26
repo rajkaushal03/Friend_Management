ea67255cc111c51fe55b7aef5090a87f93628344
Swayam Kaushal <101446225+rajkaushal03@users.noreply.github.com>
2025-03-26 21:05:26 +0530

    add delete get

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import UserGrid from "./component/UserGrid";
 import { useState } from "react";
 
 // updated this after recording. Make sure you do the same so that it can work in production
-export const BASE_URL = "http://127.0.0.1:5000/api/"
+export const BASE_URL = "http://127.0.0.1:5000/api"
 
 function App() {
 	const [users, setUsers] = useState([]);
